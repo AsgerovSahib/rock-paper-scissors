f81a0976936dee99403a965b8b24b90d13b28a79
AsgerovSahib <asgerov.sako@gmail.com>
2023-08-22 01:46:23 +0400

    add querySelectors

---
+++ main.js
@@ -0,0 +1,19 @@
+//* object
+
+//* querySelector
+
+//^ enemyPlayer querySelector
+enemyPlayerName = document.querySelector("enemy-playerName");
+enemyPlayerStatus = document.querySelector("enemy-playerStatus");
+enemyPlayerPonit = document.querySelector("enemy-playerPonit");
+enemyPlayerGameAbout = document.querySelector("enemy-player-game-about");
+//^ myPlayer querySelector
+myPlayerName = document.querySelector("my-playerName");
+myPlayerStatus = document.querySelector("my-playerStatus");
+myPlayerPonit = document.querySelector("my-playerPonit");
+
+//* addEventListener
+
+
+
+
